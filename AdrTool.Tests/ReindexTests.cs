e54d56e6398eccf03222167309321bdc05e0402c
Andreas Regner <andreas@regner.dev>
2024-02-20 13:35:30 +0100

    Add support for interesting paths

--- AdrTool.Tests/ReindexTests.cs
+++ AdrTool.Tests/ReindexTests.cs
@@ -43,5 +43,57 @@ namespace AdrTool.Tests
 | 0001 | document title | [Link](docs/0001-document title.md) |"),
                 Times.Once);
         }
+
+        [Fact]
+        public async Task Verify_Complex()
+        {
+            // arrange
+            RecordManager recordManager = new RecordManager(InputOutputUtilsMock.Object, "z:\\Unit%20-%20Test");
+            InputOutputUtilsMock
+                .Setup(mock => mock.GetFiles("z:\\Unit%20-%20Test\\docs"))
+                .Returns(new[] { "z:\\Unit%20-%20Test\\docs\\0001-document title.md" });
+            InputOutputUtilsMock
+                .Setup(mock => mock.GetFiles("z:\\Unit%20-%20Test\\docs\\subfolder"))
+                .Returns(new[] { "z:\\Unit%20-%20Test\\docs\\subfolder\\0001-document title-2.md" });
+            InputOutputUtilsMock
+                .Setup(mock => mock.GetDirectories("z:\\Unit%20-%20Test\\docs"))
+                .Returns(new[] { "z:\\Unit%20-%20Test\\docs\\subfolder" });
+
+            // act
+            await recordManager.ReindexAsync();
+
+            // assert
+            InputOutputUtilsMock.Verify(
+                mock => mock.GetFiles("z:\\Unit%20-%20Test\\docs"),
+                Times.Once);
+            InputOutputUtilsMock.Verify(
+                mock => mock.GetDirectories("z:\\Unit%20-%20Test\\docs"),
+                Times.Once);
+            InputOutputUtilsMock.Verify(
+                mock => mock.GetFiles("z:\\Unit%20-%20Test\\docs\\subfolder"),
+                Times.Once);
+            InputOutputUtilsMock.Verify(
+                mock => mock.GetDirectories("z:\\Unit%20-%20Test\\docs\\subfolder"),
+                Times.Once);
+            InputOutputUtilsMock.Verify(
+                mock => mock.WriteFileAsync(
+                    "z:\\Unit%20-%20Test\\docs\\..\\docs.md",
+                    @"# Index
+
+| Number | Title | Link |
+| ---:| --- | --- |
+| | subfolder | [Link](docs/subfolder.md) |
+| 0001 | document title | [Link](docs/0001-document title.md) |"),
+                Times.Once);
+            InputOutputUtilsMock.Verify(
+                mock => mock.WriteFileAsync(
+                    "z:\\Unit%20-%20Test\\docs\\subfolder\\..\\subfolder.md",
+                    @"# Index
+
+| Number | Title | Link |
+| ---:| --- | --- |
+| 0001 | document title 2 | [Link](subfolder/0001-document title-2.md) |"),
+                Times.Once);
+        }
     }
 }
